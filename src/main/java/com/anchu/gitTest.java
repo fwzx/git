355c005acd18e79c6f6fcaa62193997fc820f35d
Wzx <Wzx@qq.com>
2023-07-20 17:09:18 +0800

    hot-fix commit

--- src/main/java/com/anchu/gitTest.java
+++ src/main/java/com/anchu/gitTest.java
@@ -7,6 +7,7 @@ public class gitTest {
         System.out.println("version3");
         System.out.println("version4");
         System.out.println("version5");
+        System.out.println("hot-fix");
 
     }
 }
